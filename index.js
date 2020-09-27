a5ce1c0a42a65e8785ced7a696f043b9a232c210
Manisha Khatri <71606856+ManishaKhatri-class@users.noreply.github.com>
2020-09-27 16:37:54 +0530

    Adding files to new branch

---
+++ index.js
@@ -0,0 +1,5 @@
+function display() {
+    document.getElementById("image").style.display = "block";
+    document.getElementById("image").style.border = "1px solid blue";
+    document.getElementById("image").style.borderRadius = "100%";
+}
