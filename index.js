cd97cfe46c09482f86322d6bfeb878abb59378cf
Manisha Khatri <71606856+ManishaKhatri-class@users.noreply.github.com>
2020-09-28 17:51:29 +0530

    Bootstrap Changes

--- index.js
+++
@@ -1,5 +0,0 @@
-function display() {
-    document.getElementById("image").style.display = "block";
-    document.getElementById("image").style.border = "1px solid blue";
-    document.getElementById("image").style.borderRadius = "100%";
-}
